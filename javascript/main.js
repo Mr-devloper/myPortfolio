501c801da41b6cb629dc3f51d2fac0775bff385a
Mr-devloper <74835904+Mr-devloper@users.noreply.github.com>
2022-11-15 23:40:09 +0600

    Update Side Menu Button Mobile View

--- javascript/main.js
+++ javascript/main.js
@@ -61,20 +61,27 @@ function typeWriter(){
 setTimeout( typeWriter, 1000);
 }
 {
-    
+    const e = document.getElementById("menuExpand")
     let x = document.getElementsByClassName("side-menu")
     let i = 0;
     function toggle(){
        if (i === 0) {
         x[0].style.display = 'block';
+        e.innerHTML = "&times"
+        e.style.fontSize = "xx-large"
         i = 1;
        } else {
         x[0].style.display = 'none';
+        e.innerHTML = "☰";
+        e.style.fontSize = "x-large"
         i = 0;
        }
       
     } 
     function cls(){
         x[0].style.display = 'none';
+        e.innerHTML = "☰";
+        e.style.fontSize = "x-large"
+        i = 0;
        }
 }
