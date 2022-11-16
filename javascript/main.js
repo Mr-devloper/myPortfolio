f836a494375390feef4aec015e5cc18a9966d375
Mr-devloper <74835904+Mr-devloper@users.noreply.github.com>
2022-11-16 07:14:28 +0600

    removed clock display

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,7 @@
-console.log("main.js connected");
+{
+    const status = "connected"
+    console.log(status);
+}
 var splashScreen = document.querySelector('.splash');
 splashScreen.addEventListener('click',()=>{
     splashScreen.style.opacity = 0;
@@ -6,46 +9,6 @@ splashScreen.addEventListener('click',()=>{
         splashScreen.classList.add('hidden')
     }, 610)
 })
-
-function showTime(){
-    var date = new Date();
-    var h = date.getHours();
-    var m = date.getMinutes();
-    var s = date.getSeconds();
-    var session = "AM";
-    
-
-if (h == 0) {
-    h = 12;
-}
-
-if ( h > 12) {
-
-    h = h-12 ;
-    session = "PM";
-}
-
-h = ( h < 10) ? "0" + h : h;
-m = ( m < 10) ? "0" + m : m;
-s = ( s < 10) ? "0" + s : s;
-
-var time = h + ":" + m + ":" + s + " " + session;
-document.getElementById("MyClockDisplay").innerText = time;
-document.getElementById("MyClockDisplay").textContent = time;
-
-setTimeout(showTime, 1000);
-
-
-
-
-
-
-
-}
-
-
-showTime();
-
 {
     // Type writer animation 
 let i = 0;
